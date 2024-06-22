05b45a9af24b2110cc72366443750d5321a13f64
eloza <lozaedward@gmail.com>
2024-06-22 16:50:02 -0700

    Add src folder, pom.xml, mvnw, help.md

---
+++ src/test/java/com/ECL062024/ToolRental/ToolRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ECL062024.ToolRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
