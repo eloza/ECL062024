b346ac5b498faf3cc5572265707cd78a6b213b54
eloza <lozaedward@gmail.com>
2024-06-24 22:52:44 -0700

    Add logging to Tool class

--- src/main/java/com/ECL062024/ToolRental/model/Tool.java
+++ src/main/java/com/ECL062024/ToolRental/model/Tool.java
@@ -1,7 +1,10 @@
 package com.ECL062024.ToolRental.model;
 
-public class Tool {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Tool {
+    private static final Logger logger = LoggerFactory.getLogger(Tool.class);
     private final String toolCode;
     private final String toolType;
     private final String toolBrand;
@@ -20,34 +23,45 @@ public class Tool {
         this.weekdayCharge = weekdayCharge;
         this.weekendCharge = weekendCharge;
         this.holidayCharge = holidayCharge;
+
+        logger.info("Tool created: {} - {} (Brand: {}, Daily Charge: {}, Weekday Charge: {}, Weekend Charge: {}, " +
+                        "Holiday Charge: {})", toolCode, toolType, toolBrand, dailyCharge, weekdayCharge,
+                weekendCharge, holidayCharge);
     }
 
     // Getters
     public String getToolCode() {
+        logger.debug("Getting tool code: {}", toolCode);
         return toolCode;
     }
 
     public String getToolType() {
+        logger.debug("Getting tool type: {}", toolType);
         return toolType;
     }
 
     public String getToolBrand() {
+        logger.debug("Getting tool brand: {}", toolBrand);
         return toolBrand;
     }
 
     public double getDailyCharge() {
+        logger.debug("Getting daily charge: {}", dailyCharge);
         return dailyCharge;
     }
 
     public boolean isWeekdayCharge() {
+        logger.debug("Getting weekday charge: {}", weekdayCharge);
         return weekdayCharge;
     }
 
     public boolean isWeekendCharge() {
+        logger.debug("Getting weekend charge: {}", weekendCharge);
         return weekendCharge;
     }
 
     public boolean isHolidayCharge() {
+        logger.debug("Getting holiday charge: {}", holidayCharge);
         return holidayCharge;
     }
 
